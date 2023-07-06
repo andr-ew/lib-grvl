805056b650f0a17294f3b1b00d73ac07f4cc8b7c
andr-ew <andrewcshike@gmail.com>
2023-07-06 10:51:43 -0500

    pan defaults

--- params.lua
+++ params.lua
@@ -163,7 +163,7 @@ for chan = 1,2 do
     }
     params:add{
         type = 'control', id = 'output_pan_'..chan, name = 'output pan',
-        controlspec = cs.def{ min = -5, max = 5, default = 0, units = 'v' },
+        controlspec = cs.def{ min = -5, max = 5, default = chan==1 and -4 or 4, units = 'v' },
         action = function(v)
             engine.out_pan(chan, v/5)
 
